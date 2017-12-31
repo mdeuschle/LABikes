98430563d7be461b217ad19661827ef78b6bf3fe
Matt Deuschle <mdeuschle@gmail.com>
2017-12-30 23:43:22 -0800

    add lottie

---
+++ LABikes/View/LottieView.swift
@@ -0,0 +1,21 @@
+//
+//  LottieView.swift
+//  LABikes
+//
+//  Created by Matt Deuschle on 12/30/17.
+//  Copyright © 2017 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class LottieView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
