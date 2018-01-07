2b30a38fc65a4ad97545cc6e33b6b9cf675a2ba4
Matt Deuschle <mdeuschle@gmail.com>
2018-01-06 19:26:45 -0800

    down arrow

--- LABikes/Controller/MapPopUpVC.swift
+++ LABikes/Controller/MapPopUpVC.swift
@@ -12,8 +12,6 @@ class MapPopUpVC: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var tempLabel: UILabel!
-    @IBOutlet weak var weatherIcon: UIImageView!
 
     private var bike: Bike?
 
@@ -27,27 +25,6 @@ class MapPopUpVC: UIViewController {
         nameLabel.text = bike.name                                              
         addressLabel.text = bike.addressStreet
         self.bike = bike
-    }
-
-    func downloadWeather() {
-        APIManager.shared.performAPICall(urlString: APIManager.Router.weather.path) { (success, data) in
-            if success {
-                if let temp = DataHelper.shared.convertDataToTemperature(data: data!) {
-                    DispatchQueue.main.async {
-                        self.tempLabel.text = temp
-                    }
-                }
-                if let url = DataHelper.shared.convertDataToIconURL(data: data!) {
-                    APIManager.shared.performAPICall(urlString: url, handler: { (iconSuccess, iconData) in
-                        if iconSuccess {
-                            DispatchQueue.main.async {
-                                self.weatherIcon.image = UIImage(data: iconData!)
-                            }
-                        }
-                    })
-                }
-            }
-        }
     }
     
     @IBAction func directionsTapped(_ sender: UIButton) {
